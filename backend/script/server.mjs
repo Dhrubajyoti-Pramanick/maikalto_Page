e4884539222794b6b0a5bdeded53730de15bc4af
Dhrubajyoti-Pramanick <dhrubajyoti@itobuz.com>
2023-03-30 18:48:00 +0530

    4th commit

--- backend/script/server.mjs
+++ backend/script/server.mjs
@@ -1,26 +1,38 @@
-// import * as fs from "fs/promises";
+import * as fsPromise from "fs/promises";
 import * as fs from "fs";
 import * as http from 'http';
-let route;
-let data = {
-    1:"hello world",
-};
+
 const port = 8000;
-let file =fs.readFileSync('../data.json', "utf8");
+let file = fs.readFileSync('../data.json', "utf8");
 let database = JSON.parse(file);
 
 http
-  .createServer(function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers","*"); // allows client to access data from server
-    res.writeHead(200, { "Content-type": "text/html" });
-    let flag = 0;
-    if (flag === 0) {
-      res.write(JSON.stringify(database));
-      console.log(data);
-      res.end();
-    }
-  })
-  .listen(port, function () {
-    console.log(`Server started at port ${port}`);
-  });
+    .createServer(function (req, res) {
+        try {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Headers", "*"); // allows client to access data from server
+            res.writeHead(200, { "Content-type": "text/html" });
+            let flag = 0;
+            if (flag === 0) {
+                res.write(JSON.stringify(database));
+                res.end();
+            }
+            let formData = [];
+            req.on("data", (formDataPieces) => {
+                formData.push(formDataPieces);
+            });
+            req.on("end", () => {
+                let formdata = Buffer.concat(formData).toString();
+                let getData = async () => {
+                    await fsPromise.appendFile("./formData.txt", formdata);
+                }
+                getData();
+            })
+            res.end();
+        } catch (err) {
+            console.log(err);
+        }
+    })
+    .listen(port, function () {
+        console.log(`Server started at port ${port}`);
+    });
--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,20 +1,93 @@
 let swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 //data fetch
 async function getServerData() {
     const response = await fetch(
         `http://localhost:8000`
-      ).then(function (res) {
+    ).then(function (res) {
         console.log(res);
         return res.json();
-      });
-      console.log(response);
-    //   document.getElementById("vacation").innerHTML = response.section2.text;
+    });
+    console.log(response);
+    console.log(response.carousel.carousel3.roomType);
+    //searchRoom section
+    document.getElementById("room").style.backgroundImage = `url(${response.searchRoom.img})`;
+
+    //welcomeNote section
+    document.getElementById("welcomeNoteImg1").innerHTML = `<img class="w-75" src='${response.welcomeNote.img1}' alt="bedroomNotFound"/>`
+    document.getElementById("welcomeNoteImg2").innerHTML = `<img class="w-75" src='${response.welcomeNote.img2}' alt="bedroomNotFound"/>`
+    document.getElementById("welcomeNoteImg3").innerHTML = `<img class="w-75" src='${response.welcomeNote.img3}' alt="bedroomNotFound"/>`
+
+    //carousel section
+    document.getElementById("carousel1").innerHTML = `<img class="imgCarousel" src='${response.carousel.carousel1.img}' alt="bedroomNotFound"/>`
+    document.getElementById("carousel2").innerHTML = `<img class="imgCarousel" src='${response.carousel.carousel2.img}' alt="bedroomNotFound"/>`
+    document.getElementById("carousel3").innerHTML = `<img class="imgCarousel" src='${response.carousel.carousel3.img}' alt="bedroomNotFound"/>`
+    
+    // next button carousel
+    document.getElementById("nextButton").addEventListener('click', () => {
+        console.log(swiper.activeIndex);
+        if (swiper.activeIndex === 0) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel1.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel1.price}`;
+        } else if (swiper.activeIndex === 1) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel2.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel2.price}`;
+        } else if (swiper.activeIndex === 2) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel3.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel3.price}`;
+        }
+    })
+    // prev button carousel
+    document.getElementById("prevButton").addEventListener('click', () => {
+        console.log(swiper.activeIndex);
+        if (swiper.activeIndex === 0) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel1.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel1.price}`;
+        } else if (swiper.activeIndex === 1) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel2.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel2.price}`;
+        } else if (swiper.activeIndex === 2) {
+            document.getElementById("roomType").innerHTML = `${response.carousel.carousel3.roomType}`;
+            document.getElementById("price").innerHTML = `${response.carousel.carousel3.price}`;
+        }
+    })
 }
+getServerData();
+let form = {
+    "checkIn": "",
+    "checkOut": "",
+    "adults": "",
+    "children": ""
+}
+// form submission
+function formSubmit(event) {
+    event.preventDefault();
+    console.log("hello");
+    console.log(document.getElementById("checkIn").value);
+    console.log(document.getElementById("checkOut").value);
+    console.log(document.getElementById("adults").value);
+    console.log(document.getElementById("children").value);
+    form.checkIn = document.getElementById("checkIn").value;
+    form.checkOut = document.getElementById("checkOut").value;
+    form.adults = document.getElementById("adults").value;
+    form.children = document.getElementById("children").value;
+    console.log(form);
 
-
-// getServerData();
+    fetch("http://localhost:8000/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(form),
+  }).then(() => {
+    alert("Form submitted!");
+    document.getElementById("checkIn").value = null;
+    document.getElementById("checkOut").value = null;
+    document.getElementById("adults").value = "0";
+    document.getElementById("children").value = "0";
+  });
+}
